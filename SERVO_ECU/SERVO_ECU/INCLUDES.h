6746eaad9b0da902b422fc509d4420015518e8cf
Mahmoud_Saad <msammamsa10@gmail.com>
2019-03-25 13:09:33 +0200

    01servo

---
+++ SERVO_ECU/SERVO_ECU/INCLUDES.h
@@ -0,0 +1,26 @@
+/*
+ * INCLUDES.h
+ *
+ * Created: 30/11/2018 04:41:53
+ *  Author: Mahmoud
+ */ 
+
+
+#ifndef INCLUDES_H_
+#define INCLUDES_H_
+#ifndef F_CPU
+#define  F_CPU 8000000UL
+#endif
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include "common.h"
+#include "UART.h"
+#include "hal_PWM.h"
+#include "hal_PWM_CFG.h"
+
+
+
+
+#endif /* INCLUDES_H_ */
