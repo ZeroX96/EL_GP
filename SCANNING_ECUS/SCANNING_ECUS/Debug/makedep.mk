257c9cd496c924ad3724ae98d2ec2943bba80cbc
Mahmoud_Saad <msammamsa10@gmail.com>
2019-03-25 11:05:46 +0200

    00scanning

---
+++ SCANNING_ECUS/SCANNING_ECUS/Debug/makedep.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+HAL\EXTERNAL_INTERRUPTS\external_interrupts.c
+
+main.c
+
+RTOS\croutine.c
+
+RTOS\event_groups.c
+
+RTOS\heap_1.c
+
+RTOS\list.c
+
+RTOS\port.c
+
+RTOS\queue.c
+
+RTOS\tasks.c
+
+RTOS\timers.c
+
---
+++ SCANNING_ECUS/SCANNING_ECUS/main.c
@@ -0,0 +1,18 @@
+/*
+ * SCANNING_ECUS.c
+ *
+ * Created: 3/22/2019 4:27:30 AM
+ * Author : MSA
+ */ 
+
+#include "includes.h"
+
+
+int main(void)
+{
+	
+    while (1) 
+    {
+    }
+}
+
